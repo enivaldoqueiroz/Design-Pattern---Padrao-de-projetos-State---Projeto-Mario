e22c038c17deeb6620e4bb2b5e864e07cba78bb8
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2020-09-27 15:52:44 -0300

    Criando a classe Principal para realizar os teste de estado

---
+++ src/patternstate/Principal.java
@@ -0,0 +1,17 @@
+package patternstate;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Mario mario = new Mario();
+		
+		mario.pegarCogumelo(); 	//Mario grande
+		mario.pegarCogumelo(); 	//Ganhou 1000 pontos
+		mario.pegarFlor();		//Mario grande com fogo
+		mario.levarDano();		//Mario grande
+		mario.levarDano();		//Mario pequeno
+
+	}
+
+}
